9729696fb2e1af8455241636e71352fa574130cf
Mateusz Margos <mateuszmargos@gmail.com>
2018-07-24 22:16:41 +0200

    feture: Added ElementsList component, added footer in the MainWindow component.

---
+++ src/ElementsList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class ElementsList extends Component {
+    render() {
+        return (
+            <div className='elements-list-container'>
+                <ul>
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ElementsList
--- src/MainWindow.js
+++ src/MainWindow.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import ElementsList from './ElementsList';
 
 class MainWindow extends Component {
     render() {
         return (
-            <header>
-                <h1 className='header'>Neighborhood Map</h1>
-            </header>
+            <main>
+                <header>
+                    <h1 className='header'>Neighborhood Map</h1>
+                </header>
+                <ElementsList />
+                <footer>
+                    <p className='footer'>Made with <b className='far fa-heart'></b> by Mateusz Margos</p>
+                </footer>
+            </main>
         )
     }
 }
