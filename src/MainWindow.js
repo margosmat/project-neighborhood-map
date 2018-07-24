33389852b85f85c0d953472d1b459e70e4485541
Mateusz Margos <mateuszmargos@gmail.com>
2018-07-24 19:13:26 +0200

    feature: Code cleanup, added MainWindow Component with header.

---
+++ src/MainWindow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MainWindow extends Component {
+    render() {
+        return (
+            <header>
+                <h1 className='header'>Neighborhood Map</h1>
+            </header>
+        )
+    }
+}
+
+export default MainWindow
