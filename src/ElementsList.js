c3240e4e0301ce9813606f3dbe15669c5fb486c7
Mateusz Margos <mateuszmargos@gmail.com>
2018-07-25 19:20:07 +0200

    feature: Added button that hides/opens elements list.

--- src/ElementsList.js
+++ src/ElementsList.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import $ from 'jquery'
 
 class ElementsList extends Component {
     render() {
         return (
             <div className='elements-list-container'>
+                <button 
+                    className='nav-button'
+                    onClick={() => {
+                        $('.elements-list-container').toggleClass('elements-list-opened');
+                        $('.nav-button').toggleClass('elements-list-opened');
+                    }}>
+                    <i className='fas fa-bars'></i>
+                </button>
                 <input
                     className='filter-input'
                     type='text'
