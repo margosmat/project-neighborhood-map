ceb76f3c02d8369063ba8517a6f61a7501092068
margosmat <mateuszmargos@gmail.com>
2019-02-23 19:00:52 +0100

    Updates

---
+++ precache-manifest.a51211edafb70f08c54a6a3c1d4d56ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdd335d825c9871c0c12eba6538f27d2",
+    "url": "/project-neighborhood-map/static/media/powered-by-foursquare-grey.cdd335d8.png"
+  },
+  {
+    "revision": "47cd880a39aab7ae6d44",
+    "url": "/project-neighborhood-map/static/js/runtime~main.47cd880a.js"
+  },
+  {
+    "revision": "e5ce201e78bd83bc92f0",
+    "url": "/project-neighborhood-map/static/js/main.e5ce201e.chunk.js"
+  },
+  {
+    "revision": "3849be0ae8319d9cf26d",
+    "url": "/project-neighborhood-map/static/js/2.3849be0a.chunk.js"
+  },
+  {
+    "revision": "e5ce201e78bd83bc92f0",
+    "url": "/project-neighborhood-map/static/css/main.bf92461f.chunk.css"
+  },
+  {
+    "revision": "eae43162a02a203aadb5dfa18fb587ae",
+    "url": "/project-neighborhood-map/index.html"
+  }
+];
